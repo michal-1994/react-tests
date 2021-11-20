7749e4d96bd61a54987c8aeb894f27ff4af65a00
Michal <michalgrzegorczyk04@gmail.com>
2021-11-20 15:29:58 +0100

    Add mocks to tests

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 
+import Output from './Output';
+import Async from './Async';
+
 const Greeting = () => {
     const [changedText, setChangedText] = useState(false);
 
@@ -10,10 +13,10 @@ const Greeting = () => {
     return (
         <div>
             <h2>Hello World</h2>
-            {!changedText && <p>It's good to see you</p>}
-            {/* <p>It's good to see you</p> */}
-            {changedText && <p>Changed!</p>}
+            {!changedText && <Output>It's good to see you</Output>}
+            {changedText && <Output>Changed!</Output>}
             <button onClick={changeTextHandler}>Change Text!</button>
+            <Async />
         </div>
     );
 }
